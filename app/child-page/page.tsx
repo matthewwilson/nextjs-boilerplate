e83972f31b1995ed6367dbaa7afa64af32c63d2a
Matthew Wilson <matthew.wilson@instil.co>
2023-10-09 21:08:18 +0100

    Fixing styles

--- app/child-page/page.tsx
+++ app/child-page/page.tsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 export default async function ChildPage() {
     const timestamp = await getCloudflareTimestamp();
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl font-bold text-center">{timestamp}</h1>
-            <p>Child Page Fetched at {new Date().toISOString()}</p>
-            <Link href="/">Home Page</Link>
-            <Link href="/edge-page">Edge Page</Link>
+        <main className="flex flex-col items-center justify-between p-24">
+            <div className="p-4">
+                <h1 className="text-4xl font-bold text-center">{timestamp}</h1>
+                <p>Fetched at {new Date().toISOString()}</p>
+            </div>
+            <div className="p-4 flex flex-col items-center">
+                <Link href="/">Home Page</Link>
+                <Link href="/edge-page">Edge Page</Link>
+            </div>
         </main>
     )
 }
--- app/edge-page/page.tsx
+++ app/edge-page/page.tsx
@@ -6,11 +6,15 @@ export const runtime = "edge";
 export default async function EdgePage() {
     const timestamp = await getCloudflareTimestamp();
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl font-bold text-center">{timestamp}</h1>
-            <p>Edge Page Fetched at {new Date().toISOString()}</p>
-            <Link href="/">Home Page</Link>
-            <Link href="/child-page">Child Page</Link>
+        <main className="flex flex-col items-center justify-between p-24">
+            <div className="p-4">
+                <h1 className="text-4xl font-bold text-center">{timestamp}</h1>
+                <p>Fetched at {new Date().toISOString()}</p>
+            </div>
+            <div className="p-4 flex flex-col items-center">
+                <Link href="/">Home Page</Link>
+                <Link href="/child-page">Child Page</Link>
+            </div>
         </main>
     )
 }
