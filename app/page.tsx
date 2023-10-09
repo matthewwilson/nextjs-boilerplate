4b9b94382da4c79d200b03deec35543b4a084f33
Matthew Wilson <matthew.wilson@instil.co>
2023-10-09 20:59:00 +0100

    Removing no cache version

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {getCloudflareTimestamp, getNoCacheCloudflareTimestamp} from "@/app/service";
+import {getCloudflareTimestamp} from "@/app/service";
 import Link from "next/link";
 
 export default async function Home() {
--- app/service.ts
+++ app/service.ts
@@ -8,16 +8,6 @@ export async function getCloudflareTimestamp(): Promise<string> {
     return await parseTimestamp(result);
 }
 
-export async function getNoCacheCloudflareTimestamp(): Promise<string> {
-    const result = await fetch("https://1.1.1.1/cdn-cgi/trace", {
-        next: {
-           revalidate: 0,
-        }
-    });
-
-    return await parseTimestamp(result);
-}
-
 async function parseTimestamp(result: Response) {
     const body = await result.text();
 
