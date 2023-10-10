4bad01aa842a4c7202ff52768cbb627b85607c7e
Matthew Wilson <matthew.wilson@instil.co>
2023-10-10 02:29:57 +0100

    Revalidating on edge

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import {NextRequest, NextResponse} from 'next/server'
 import { revalidateTag } from 'next/cache'
 
+export const runtime = "edge";
+
 export async function POST(request: NextRequest) {
     const secret = request.nextUrl.searchParams.get('secret')
 
