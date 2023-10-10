63ff6f57f67ca04c7824d5b4567ca2e82abab96d
Matthew Wilson <matthew.wilson@instil.co>
2023-10-10 17:11:18 +0100

    Adding middleware logging

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,21 @@ const supportedLanguages: string[] = ["en-GB"];
 export function middleware(request: NextRequest): NextResponse {
     const url = request.nextUrl.clone();
     const pathname = request.nextUrl.pathname;
+
+    console.log("Pathname", pathname);
+
     const pathnameIsMissingLocale = supportedLanguages.every(
         (language) => !pathname.startsWith(`/${language}/`) && pathname !== `/${language}`,
     );
 
     if (pathnameIsMissingLocale) {
+        console.log("Redirecting to default locale");
         const locale = "en-GB"
         url.pathname = buildUrlPath(request, locale);
         return NextResponse.redirect(url, 308);
     }
 
+    console.log("Skipping redirect");
     return NextResponse.next();
 }
 
